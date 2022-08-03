128e09ffee17d7f7edaf2ff2e72c0b93e38deaec
최슬기 <choisk09@gmail.com>
2022-08-03 15:32:37 +0900

    conflict test

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function Nav() {
-  return <h1> 최강의 팀 2</h1>;
+  return <h1> 최강의 팀 2</h1>
+  <h1><a href="http://www.naver.com"naver 바로가기</a></h1>
 }
 
 export default Nav;
