a2de2bc9c7d72df0f97bd3dea11cf8ab351d35d8
Chaedie <chaedong.im.dev@gmail.com>
2022-08-04 14:43:16 +0900

    Add : Entry Page 구현

--- src/Router.js
+++ src/Router.js
@@ -10,10 +10,13 @@ import MainChaedong from './pages/chaedong/Main/Main';
 import LoginSeulki from './pages/seulki/Login/Login';
 import MainSeulki from './pages/seulki/Main/Main';
 
+import Entry from './pages/Entry';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Entry />} />
         <Route path="/login-youngjun" element={<LoginYoungjun />} />
         <Route path="/main-youngjun" element={<MainYoungjun />} />
         <Route path="/login-chaedong" element={<LoginChaedong />} />
